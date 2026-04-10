847b2bce34fe13f3a0b001276101b0d7c09d45bc
Francisco <simoesdeoliveirafrancisco@gmail.com>
2026-04-09 22:22:44 -0300

    feat: inicializar estrutura do front-end com autenticação e dashboard
    - Adiciona configuração base do React (package.json, public, src).
    - Implementa telas de autenticação: Login, Registro, Esqueci a Senha e Verificação de Código.
    - Adiciona tela de Dashboard e alteração de senha.
    - Configura estilos globais e arquivos de teste iniciais.

---
+++ Front-end/src/features/auth/pages/login.jsx
@@ -0,0 +1,21 @@
+import "./login.css";
+
+const Login = () => {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <span>Email</span>
+          <input type="text" placeholder="Email" />
+        </div>
+        <div>
+          <span>Senha</span>
+          <input type="password" placeholder="******" />
+        </div>
+        <button>Entrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
